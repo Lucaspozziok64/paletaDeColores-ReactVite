34bc1c04fe7ac08fbe0c5c9585df80a84ffb248c
Lucasfi <lukafigueroa64@gmail.com>
2025-06-21 15:13:47 -0300

    agregamos icono de bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import FormularioColor from './components/FormularioColor'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
 
   return (
     <main className='container my-5'>
-      <section className='container bg-secondary'>
+      <section className='container bg-secondary rounded-4'>
         <FormularioColor />
       </section>
     </main>
--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -4,10 +4,10 @@ const FormularioColor = () => {
       <div className="w-100 text-center">
         <h2 className="w-100 text-light">Paleta de colores🎨</h2>
       <div className="d-flex justify-content-center align-items-center">
-        <label className="form-label">Selecciona un Color:</label>
+        <label className="form-label"><strong>Selecciona un Color:</strong></label>
         <input type="color" className="mx-3 mb-1" />
         <button type="submit" className="btn btn-success">
-          Agregar
+          <i className="bi bi-plus-circle mx-1 text-white"></i>Agregar
         </button>
       </div>
       </div>
