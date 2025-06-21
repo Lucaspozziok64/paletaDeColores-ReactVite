1d0eff24ca0350f5e2b1b30fc4ad34f7487c9ba1
Lucasfi <lukafigueroa64@gmail.com>
2025-06-21 17:39:08 -0300

    agregamos mensaje al usaurio con sweetalert2 en caso de no elegir un color valido

--- src/components/FormularioColor.jsx
+++ src/components/FormularioColor.jsx
@@ -1,29 +1,44 @@
 import { useState } from "react";
+import Swal from "sweetalert2";
 
 const FormularioColor = ({ agregarColor }) => {
-
-  const [colorInput, setColorInput] = useState('')
+  const [colorInput, setColorInput] = useState("");
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if(!colorInput.trim()) {
+    e.preventDefault();
+    if (!colorInput.trim()) {
+      Swal.fire({
+        title: "Debes elegir un color, haciendo click en el recuadro del input",
+        text: "Asi no funciona!",
+        icon: "warning",
+      });
       return;
     }
-    agregarColor(colorInput)
-    setColorInput('')
-  }
+    agregarColor(colorInput);
+    setColorInput("");
+  };
 
   return (
-    <form onSubmit={handleSubmit} className="d-flex justify-content-center align-items-center">
+    <form
+      onSubmit={handleSubmit}
+      className="d-flex justify-content-center align-items-center"
+    >
       <div className="w-100 text-center">
         <h2 className="w-100 text-light">Paleta de colores🎨</h2>
-      <div className="d-flex justify-content-center align-items-center">
-        <label className="form-label"><strong>Selecciona un Color:</strong></label>
-        <input type="color" className="mx-3 mb-1" onChange={(e)=> setColorInput(e.target.value)} value={colorInput}/>
-        <button type="submit" className="btn btn-success">
-          <i className="bi bi-plus-circle mx-1 text-white"></i>Agregar
-        </button>
-      </div>
+        <div className="d-flex justify-content-center align-items-center">
+          <label className="form-label">
+            <strong>Selecciona un Color:</strong>
+          </label>
+          <input
+            type="color"
+            className="mx-3 mb-1"
+            onChange={(e) => setColorInput(e.target.value)}
+            value={colorInput}
+          />
+          <button type="submit" className="btn btn-success">
+            <i className="bi bi-plus-circle mx-1 text-white"></i>Agregar
+          </button>
+        </div>
       </div>
     </form>
   );
