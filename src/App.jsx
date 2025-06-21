c1f95a91a1e32506a20b3d27d7ee6d77813ff5cc
Lucasfi <lukafigueroa64@gmail.com>
2025-06-21 14:39:46 -0300

    creamos el proyecto y limpiamos estructura

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <main className='container my-5'>
+      <h1>Hello World</h1>
+    </main>
+  )
+}
+
+export default App
