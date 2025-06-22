3bcf791a372f2fc819124ee7e169d7f4890cf95e
Lucasfi <lukafigueroa64@gmail.com>
2025-06-21 21:33:57 -0300

    Agregamos footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,38 @@ import CardColor from "./components/CardColor";
 import { useState } from "react";
 
 function App() {
-
   const [colores, setColores] = useState([]);
 
   const agregarColor = (nuevoColor) => {
-    const nuevo = {id: Date.now(), nombre: nuevoColor}
-    setColores([...colores, nuevo])
-  }
+    const nuevo = { id: Date.now(), nombre: nuevoColor };
+    setColores([...colores, nuevo]);
+  };
 
   const borrarCard = (id) => {
-    setColores(colores.filter(color => color.id !== id));
-  }
+    setColores(colores.filter((color) => color.id !== id));
+  };
 
   return (
-    <main className="container my-5">
-      <section className="container bg-secondary rounded-4">
-        <FormularioColor agregarColor={agregarColor} />
-      </section>
+    <>
+      <main className="container my-5">
+        <section className="container bg-secondary rounded-4">
+          <FormularioColor agregarColor={agregarColor} />
+        </section>
         <div className="container-fluid my-3 row row-cols-1 row-cols-md-3 row-cols-lg-4 g-4">
-          {colores.map((color)=> 
-          <CardColor
-            key={color.id} 
-            color={color} 
-            borrarCard={()=> borrarCard(color.id)}
-          />)}
-      </div>
-    </main>
+          {colores.map((color) => (
+            <CardColor
+              key={color.id}
+              color={color}
+              borrarCard={() => borrarCard(color.id)}
+            />
+          ))}
+        </div>
+      </main>
+      <footer className="bg-dark text-center text-white p-1">
+        <p className="mb-0">Desarrollado por <a href="">Lucas Figueroa</a>👨‍💻</p>
+        <p className="mb-0">&copy;Todos los derechos reservados</p>
+      </footer>
+    </>
   );
 }
 
